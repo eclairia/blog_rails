bac92ec0c93e9ea8266ff5246bdf77ee6208b648
= <adrien.jourdier@eemi.com>
2018-02-02 19:48:22 +0100

    The rewriting url works for front

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,11 +16,11 @@ class ArticlesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_article
-      @article = Article.find(params[:id])
+      @article = Article.find_by(slug: params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def article_params
-      params.require(:article).permit(:title, :text, :image, :language)
+      params.require(:article).permit(:title, :text, :image, :slug, :language)
     end
 end

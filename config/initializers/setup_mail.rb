7dd18faeaabc5fdf834a11a4babbad0edb1623d3
= <adrien.jourdier@eemi.com>
2018-02-03 20:15:37 +0100

    Rechange smtp production

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,9 +2,9 @@ ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 '587',
-    domain:               'heroku.com',
-    user_name:            ENV["adrien.jourdier@eemi.com"],
-    password:             ENV["arrokqbk"],
+    domain:               'gmail.com',
+    user_name:            "adrien.jourdier@eemi.com",
+    password:             "arrokqbk",
     authentication:       'plain',
     enable_starttls_auto: true
 }

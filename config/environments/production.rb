b25855e6ba8fd97928b31f0b87fe229c4079dedd
= <adrien.jourdier@eemi.com>
2018-02-03 19:55:45 +0100

    Addsetup_mail file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,16 +89,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  ActionMailer::Base.delivery_method = :smtp
-  ActionMailer::Base.smtp_settings = {
-      address:              'smtp.gmail.com',
-      port:                 587,
-      domain:               'https://fierce-sierra-61774.herokuapp.com/',
-      user_name:            ENV["adrien.jourdier@eemi.com"],
-      password:             ENV["arrokqbk"],
-      authentication:       'plain',
-      enable_starttls_auto: true
-  }
-
   config.action_mailer.default_url_options = { :host => 'https://fierce-sierra-61774.herokuapp.com/' }
 end

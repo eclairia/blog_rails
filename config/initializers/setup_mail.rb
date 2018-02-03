9a945d33181df9daef604b3504709a3d6e94a05a
= <adrien.jourdier@eemi.com>
2018-02-03 20:01:04 +0100

    Problem

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,7 +2,7 @@ ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 '587',
-    domain:               'https://fierce-sierra-61774.herokuapp.com/',
+    domain:               'heroku.com',
     user_name:            ENV["adrien.jourdier@eemi.com"],
     password:             ENV["arrokqbk"],
     authentication:       'plain',

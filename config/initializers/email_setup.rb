ce121b3b0e81f603ab6d21a99182f2338b73f4d1
= <adrien.jourdier@eemi.com>
2018-02-03 15:22:47 +0100

    Bug smtp prod

--- config/initializers/email_setup.rb
+++ config/initializers/email_setup.rb
@@ -1,10 +1,10 @@
-# ActionMailer::Base.delivery_method = :smtp
-# ActionMailer::Base.smtp_settings = {
-#   address:              'smtp.gmail.com',
-#   port:                 587,
-#   domain:               'https://fierce-sierra-61774.herokuapp.com/',
-#   user_name:            ENV["adrien.jourdier@eemi.com"],
-#   password:             ENV["arrokqbk"],
-#   authentication:       'plain',
-#   enable_starttls_auto: true
-# }
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'https://fierce-sierra-61774.herokuapp.com/',
+  user_name:            ENV["adrien.jourdier@eemi.com"],
+  password:             ENV["arrokqbk"],
+  authentication:       'plain',
+  enable_starttls_auto: true
+}

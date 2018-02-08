d8eca55115a92d841caa1f5cc2821033c2811965
= <adrien.jourdier@eemi.com>
2018-02-08 13:21:25 +0100

    Test facebook login heroku

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,8 @@ Rails.application.routes.draw do
   get '/about', to: 'static#index'
 
   devise_for :users, controllers: {
-      sessions: 'users/sessions'
+      sessions: 'users/sessions',
+      omniauth_callbacks: 'users/omniauth_callbacks'
   }
 
   resources :contacts, only: [:new, :create]
